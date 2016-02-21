aba51b51cca0091830eff9d0ab47e79cc361fc00
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-21 12:26:00 +0000

    Adding config file

---
+++ config/taxonomies.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'connection' => null,
+        'prefix'     => null,
+    ],
+
+    'categories' => [
+        'table' => 'categories',
+        'model' => \Arcanedev\Taxonomies\Models\Category::class,
+    ],
+];
