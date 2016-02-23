0c6671ceec7a055fe39a2f7db1cc7d749ed3b436
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-23 13:35:25 +0000

    Adding Categorizable trait

---
+++ src/Traits/Categorizable.php
@@ -0,0 +1,89 @@
+<?php namespace Arcanedev\Taxonomies\Traits;
+
+use Arcanedev\Taxonomies\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class     Categorizable
+ *
+ * @package  Arcanedev\Taxonomies\Traits
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ *
+ * @property  \Illuminate\Database\Eloquent\Collection  $categories
+ *
+ * @method    \Illuminate\Database\Eloquent\Relations\MorphToMany  morphToMany(string $related, string $name, string $table = null, string $foreignKey = null, $otherKey = null, $inverse = false)
+ */
+trait Categorizable
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Relationships
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
+    public function categories()
+    {
+        return $this->morphToMany(Category::class,
+            config('taxonomies.categories.morph.name', 'categorizable'),
+            config('taxonomies.categories.morph.table', 'categories_relations')
+        );
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function categoriesList()
+    {
+        return $this->categories()->lists('name', 'id')->toArray();
+    }
+
+    /**
+     * @param  array  $categories
+     */
+    public function categorize($categories)
+    {
+        foreach ($categories as $category) {
+            $this->addCategory($category);
+        }
+    }
+
+    /**
+     * @param  array  $categories
+     */
+    public function uncategorize($categories)
+    {
+        foreach ($categories as $category) {
+            $this->removeCategory($category);
+        }
+    }
+
+    /**
+     * @param  array  $categories
+     */
+    public function recategorize($categories)
+    {
+        $this->categories()->sync([]);
+
+        $this->categorize($categories);
+    }
+
+    /**
+     * @param Model $category
+     */
+    public function addCategory(Model $category)
+    {
+        if ( ! $this->categories->contains($category->getKey())) {
+            $this->categories()->attach($category);
+        }
+    }
+
+    /**
+     * @param Model $category
+     */
+    public function removeCategory(Model $category)
+    {
+        $this->categories()->detach($category);
+    }
+}
