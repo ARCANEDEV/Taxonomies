c3e0a3a79498929e982be7b5034586fbbbd740e3
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-23 13:46:23 +0000

    Updating Category Model

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -14,6 +14,11 @@ class Category extends Node
      |  Properties
      | ------------------------------------------------------------------------------------------------
      */
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
     /* ------------------------------------------------------------------------------------------------
